71cf3c7fbd7d803bfda5b34596ca7a8b7e486e6e
97starsnavercom <31894223+97starsnavercom@users.noreply.github.com>
2017-12-14 15:50:57 +0900

    스프래시 엑티비티 초기

--- app/src/main/java/com/hansung/android/project3/MainActivity.java
+++ app/src/main/java/com/hansung/android/project3/MainActivity.java
@@ -1,13 +1,36 @@
 package com.hansung.android.project3;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mCountDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCountDown = (ImageView) findViewById(R.id.countdown);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+
+        startCountDownFrameAnimation();
+
+
+    }
+
+    private void startCountDownFrameAnimation() {
+        mCountDown.setBackgroundResource(R.drawable.frame_anim);
+        AnimationDrawable countdownAnim = (AnimationDrawable) mCountDown.getBackground();
+        countdownAnim.start();
     }
 }
